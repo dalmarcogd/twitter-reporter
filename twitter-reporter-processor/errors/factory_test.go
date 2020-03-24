a3047f70a204bb5abd0483ec53afb9c3c95e6445
Guilherme Dalmarco <guilherme.dalmarco@hbsis.com.br>
2020-03-24 14:27:16 -0300

    tests

--- twitter-reporter-processor/errors/factory_test.go
+++ twitter-reporter-processor/errors/factory_test.go
@@ -2,16 +2,14 @@ package errors
 
 import (
 	"github.com/stretchr/testify/assert"
-	"net/http"
 	"testing"
 )
 
 func TestError_Error(t *testing.T) {
-	assert.NotEmpty(t, NewError(http.StatusInternalServerError, "error", nil).Error())
+	assert.NotEmpty(t, NewError( "error", nil).Error())
 }
 
 func TestNewError(t *testing.T) {
-	err := NewError(http.StatusInternalServerError, "error", nil)
-	assert.Equal(t, http.StatusInternalServerError, err.StatusCode)
+	err := NewError( "error", nil)
 	assert.Equal(t, "error", err.Description)
 }
--- twitter-reporter-processor/errors/rabbit_test.go
+++ twitter-reporter-processor/errors/rabbit_test.go
@@ -2,11 +2,10 @@ package errors
 
 import (
 	"github.com/stretchr/testify/assert"
-	"net/http"
 	"testing"
 )
 
 func TestNewRabbitConnectionError(t *testing.T) {
 	err := NewRabbitConnectionError()
-	assert.Equal(t, http.StatusBadRequest, err.StatusCode)
+	assert.Equal(t, "Error when connect to Rabbit.", err.Description)
 }
